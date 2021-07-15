2e6f4b8ef0066d36e56ee06f222cf73ce6ef6bde
sakib hasan <sakib686822@gmail.com>
2021-07-15 13:04:53 +0600

    reading the variable resistor to control the radius of a circle

--- new/public/app.js
+++ new/public/app.js
@@ -2,15 +2,68 @@ let socket=io();
 const onBtn=document.getElementById("ON");
 const offBtn=document.getElementById("OFF")
 const led=document.getElementById("led")
+const btn=document.getElementById("s")
 
 onBtn.addEventListener("click",(e)=>{
     // console.log("turn the light ON");
-    led.style.backgroundColor="green"
-socket.emit("LIGHT-ON","1")
-
+    led.style.backgroundColor="green" 
+    socket.emit("LIGHT-ON","1") 
 })
 offBtn.addEventListener("click",()=>{
     // console.log("turn the light OFF");
     socket.emit("LIGHT-OFF","0")
     led.style.backgroundColor="red"
 })
+
+
+//pot
+
+
+
+//canvas stufff
+
+// const canvas=document.getElementById("canvas")
+// const cWidth=canvas.width
+// const cHeight=canvas.height
+// const ctx=canvas.getContext("2d")
+// // setInterval(() => { 
+    // //         circleRadius+=1
+    // //         drawCricle(cWidth/2,cHeight/2,circleRadius)
+    // //         console.log(circleRadius);
+    // //     }, 200);
+    // //function to draw circle on the canvas
+    // function drawCricle(x,y,r){
+        //    ctx.beginPath()
+        //    ctx.arc(x,y,r,0,Math.PI*2)
+        //    ctx.stroke()
+        // }
+        
+
+
+
+        const potValue=document.getElementById("value");
+        let radiusFromArduino;
+        socket.on("potValue",(val)=>{
+            // console.log(val);
+            potValue.innerText=val;
+            radiusFromArduino=val;
+        })
+
+
+        //p5 js for canvas
+        let radius=60
+        // btn.addEventListener("click",()=>{
+            //     radius+=5
+            // })
+            function setup(){
+                createCanvas(300,300)
+            }
+            function draw(){
+                background(255)
+                noStroke()
+                fill(0,0,255)
+                //map the pot value to the radius
+               let mappedRadius= map(radiusFromArduino,0,1024,0,200)
+                circle(width/2,height/2,mappedRadius)
+    // radius+=2
+}
--- scetch/scetch.ino
+++ scetch/scetch.ino
@@ -1,14 +1,38 @@
 int ledpin=8;
-//int switchPressed=false;
+//the pot is attached to
+int analogPin=A3;
+int previousValue=0;
+int currentValue=0;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(ledpin,OUTPUT);
-   
+  
+    
 }
 
 void loop() {
+  currentValue=analogRead(analogPin);
+//  Serial.println(currentValue);
+//  delay(50);
+  if(previousValue != currentValue){
+    Serial.println(currentValue);
+    previousValue=currentValue;
+  }
+  delay(20);
+
+  
+   //send a string via serial port
+//    Serial.println("hello from arduino");
+//    delay(600);
+//   int   potValue=analogRead(analogPin);
+//   Serial.println(potValue);
+//   delay(500);
+
+    
+    
   //if something is availabel from the serial port
   if(Serial.available()>0){
     String receivedString="";
@@ -22,7 +46,11 @@ if(receivedString == "1"){
   digitalWrite(ledpin,HIGH);
   }else{
     digitalWrite(ledpin,LOW);
-    }     
+    }      
+    
+    
 }
+
+
   
 }
